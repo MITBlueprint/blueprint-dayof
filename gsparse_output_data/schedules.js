e737bec52ec19727391d7807552df21fb00fa2dd
koroket <bmasakazu@gmail.com>
2016-02-25 21:05:08 -0500

    masa/ updated after johnson -> stata

--- gsparse_output_data/schedules.js
+++ gsparse_output_data/schedules.js
@@ -46,7 +46,7 @@ module.exports = {
 			"start": "1:30 PM",
 			"end": "2:30 PM",
 			"event": "Tech talks",
-			"location": "32-141, 144, 155"
+			"location": "32-123, 141, 155"
 		},
 		{
 			"start": "2:30 PM",
