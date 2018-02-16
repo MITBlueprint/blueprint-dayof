3215ad2a6718bc431ece3e51118fb7a7e103c93e
Jason Kung <jkung101@mit.edu>
2018-02-15 20:05:38 -0500

    Updated conduct, fonts, and added speakers page

---
+++ speakers/index_data.js
@@ -0,0 +1,27 @@
+var extData = {
+	'navContext' : 'partials/nav_data.js',
+	'gaContext' : 'partials/ga_data.js'
+}
+
+var helpers = require('helpers');
+
+var APP_DIR = '../';
+
+var intData = {
+  navContext : {
+    'selectedDirectory' : helpers.currentDirectory(__dirname),
+    'applicationDirectory' : APP_DIR
+  },
+  bannerContext : {
+  	'bannerName' : 'Map'
+  },
+  semanticsContext : {
+  	'applicationDirectory' : APP_DIR
+  },
+  headerContext : {
+  	'applicationDirectory' : APP_DIR
+  }
+}
+
+module.exports.extData = extData;
+module.exports.intData = intData;
